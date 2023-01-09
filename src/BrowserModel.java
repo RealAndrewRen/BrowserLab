d1c786126c9afac7ca98221a24a993dc74c4474f
Andrew Ren <108499792+RealAndrewRen@users.noreply.github.com>
2023-01-08 20:12:23 -0500

    Initial commit

---
+++ src/BrowserModel.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class BrowserModel {
+	BrowserView view;
+	Stack<Integer> past;
+	Stack<Integer> future;
+	
+	int position;
+
+	public BrowserModel(BrowserView view) {
+		this.view = view;
+		past = new Stack<Integer>();
+		future = new Stack<Integer>();
+		position = 0;
+	}
+
+	public void followLink(int n) {
+		view.update(n);
+		past.push(position);
+		position = n;
+		future.clear();
+	}
+
+	public boolean hasBack() {
+		return !past.isEmpty();
+	}
+
+	public boolean hasForward() {
+		return !future.empty();
+	}
+
+	public void home() {
+		view.update(0);
+		past.push(position);
+		position = 0;
+		future.clear();
+	}
+
+	public void back() {
+		int i = (int)past.pop();
+		future.push(position);
+		position = i;
+		view.update(i);
+	}
+
+	public void forward() {
+		int i = (int)future.pop();
+		past.push(position);
+		position = i;
+		view.update(i);
+	}
+
+}
